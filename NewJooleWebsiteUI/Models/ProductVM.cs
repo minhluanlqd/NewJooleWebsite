cdee13abedb5f4091af1bd4b253453cefad6e6d7
LuanTran <minhluanlqd@gmail.com>
2021-07-27 09:21:47 -0400

    Add Repo, Service, Entities.

---
+++ NewJooleWebsiteUI/Models/ProductVM.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace JooleWebsite.Models
+{
+    public class ProductVM
+    {
+        public string productID { get; set; }
+        public int subCategoryID { get; set; }
+        public string productName { get; set; }
+        public string productImage { get; set; }
+        public string series { get; set; }
+        public string model { get; set; }
+        public int modelYear { get; set; }
+        public string seriesInfo { get; set; }
+        public string features { get; set; }
+
+        public ProductVM(string pid, int scid, string pn, string pi, string s, string m, int y, string si, string f)
+        {
+            productID = pid;
+            subCategoryID = scid;
+            productName = pn;
+            productImage = pi;
+            series = s;
+            model = m;
+            modelYear = y;
+            seriesInfo = si;
+            features = f;
+        }
+
+        public ProductVM()
+        {
+
+        }
+    }
+}
