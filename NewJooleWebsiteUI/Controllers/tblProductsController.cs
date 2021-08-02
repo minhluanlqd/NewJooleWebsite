9604ca00a3e693ac9d162ac827f1940db71efad3
tarun <tarun@DESKTOP-5618SP2>
2021-08-01 20:51:47 -0600

    ProductDetails included

---
+++ NewJooleWebsiteUI/Controllers/tblProductsController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using NewJooleWebsiteEntities;
+
+namespace NewJooleWebsiteUI.Controllers
+{
+    public class tblProductsController : Controller
+    {
+        private Entities1 db = new Entities1();
+
+        // GET: tblProducts
+        public ActionResult Index()
+        {
+            var tblProducts = db.tblProducts.Include(t => t.tblSubcategory);
+            return View(tblProducts.ToList());
+        }
+
+        // GET: tblProducts/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tblProduct tblProduct = db.tblProducts.Find(id);
+            if (tblProduct == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tblProduct);
+        }
+
+        // GET: tblProducts/Create
+        public ActionResult Create()
+        {
+            ViewBag.Subcategory_ID = new SelectList(db.tblSubcategories, "Subcategory_ID", "Category_ID");
+            return View();
+        }
+
+        // POST: tblProducts/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Product_ID,Subcategory_ID,Product_Name,Product_Image,Series,Model,Model_Year,Series_Info,Features")] tblProduct tblProduct)
+        {
+            if (ModelState.IsValid)
+            {
+                db.tblProducts.Add(tblProduct);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.Subcategory_ID = new SelectList(db.tblSubcategories, "Subcategory_ID", "Category_ID", tblProduct.Subcategory_ID);
+            return View(tblProduct);
+        }
+
+        // GET: tblProducts/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tblProduct tblProduct = db.tblProducts.Find(id);
+            if (tblProduct == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.Subcategory_ID = new SelectList(db.tblSubcategories, "Subcategory_ID", "Category_ID", tblProduct.Subcategory_ID);
+            return View(tblProduct);
+        }
+
+        // POST: tblProducts/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Product_ID,Subcategory_ID,Product_Name,Product_Image,Series,Model,Model_Year,Series_Info,Features")] tblProduct tblProduct)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(tblProduct).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.Subcategory_ID = new SelectList(db.tblSubcategories, "Subcategory_ID", "Category_ID", tblProduct.Subcategory_ID);
+            return View(tblProduct);
+        }
+
+        // GET: tblProducts/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tblProduct tblProduct = db.tblProducts.Find(id);
+            if (tblProduct == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tblProduct);
+        }
+
+        // POST: tblProducts/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            tblProduct tblProduct = db.tblProducts.Find(id);
+            db.tblProducts.Remove(tblProduct);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
